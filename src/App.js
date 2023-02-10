135a882e7524e89ff9b8067d74dd2d6994b7dab3
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-02-10 13:57:20 +0100

    MyNavbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from "./components/MyNavbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MyNavbar />
+    </div>
+  );
 }
 
 export default App;
